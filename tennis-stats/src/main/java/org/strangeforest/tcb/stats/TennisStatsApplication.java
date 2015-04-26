62cbe7d52bbf802588b49d43d91577ce1835b101
Mileta Cekovic <mileta.cekovic@gtech.com>
2015-04-26 23:06:10 +0200

    Tennis stats skeleton

---
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/TennisStatsApplication.java
@@ -0,0 +1,12 @@
+package org.strangeforest.tcb.stats;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class TennisStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisStatsApplication.class, args);
+	}
+}
---
+++ tennis-stats/src/test/java/org/strangeforest/tcb/stats/TennisStatsApplicationTests.java
@@ -0,0 +1,17 @@
+package org.strangeforest.tcb.stats;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.boot.test.*;
+import org.springframework.test.context.junit4.*;
+import org.springframework.test.context.web.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TennisStatsApplication.class)
+@WebAppConfiguration
+public class TennisStatsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
