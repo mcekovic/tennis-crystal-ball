8fa524bba7dc55643e9bfc5dbece6405aa7e2007
Mileta Cekovic <mileta.cekovic@igt.com>
2015-04-27 16:42:06 +0200

    Base directory for data load configurable

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennis.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennis.groovy
@@ -2,22 +2,22 @@ package org.strangeforest.tcb.dataload
 
 import groovy.sql.*
 
-BASE_DIR = 'M:\\tennis-data\\tennis_atp\\'
+def baseDir = getBaseDir('tcb.data.base-dir')
 
-db = [url:'jdbc:postgresql://localhost:5432/postgres', user:'tcb', password:'tcb', driver:'org.postgresql.Driver']
-sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
+def db = [url:'jdbc:postgresql://localhost:5432/postgres', user:'tcb', password:'tcb', driver:'org.postgresql.Driver']
+def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 sql.connection.autoCommit = false
 
 println 'Loading ATP players'
-new ATPPlayersLoader(sql).mergeFile(BASE_DIR + 'atp_players.csv')
+new ATPPlayersLoader(sql).mergeFile(baseDir + 'atp_players.csv')
 
 println '\nLoading ATP rankings'
 load {
 	def loader = new ATPRankingsLoader(sql)
 	def rows = 0
 	for (decade in ['70s', '80s', '90s', '00s', '10s'])
-		rows += loader.mergeFile(BASE_DIR + "atp_rankings_${decade}.csv")
-	rows += loader.mergeFile(BASE_DIR + "atp_rankings_current.csv")
+		rows += loader.mergeFile(baseDir + "atp_rankings_${decade}.csv")
+	rows += loader.mergeFile(baseDir + "atp_rankings_current.csv")
 }
 
 println '\nLoading ATP matches'
@@ -25,9 +25,9 @@ load {
 	def loader = new ATPMatchesLoader(sql)
 	def rows = 0
 	for (year in 1968..2014)
-		rows += loader.loadFile(BASE_DIR + "atp_matches_${year}.csv")
+		rows += loader.loadFile(baseDir + "atp_matches_${year}.csv")
 	def year = 2015
-	rows += loader.mergeFile(BASE_DIR + "atp_matches_${year}.csv")
+	rows += loader.mergeFile(baseDir + "atp_matches_${year}.csv")
 }
 
 def load(loader) {
@@ -40,3 +40,11 @@ def load(loader) {
 	println "Total rows: $rows in $seconds s ($rowsPerSecond row/s)"
 }
 
+def getBaseDir(property) {
+	def baseDir = System.properties[property]
+	if (!baseDir)
+		throw new IllegalArgumentException('No ATP Tennis data base directory is set, please specify it in tcb.data.base-dir system property.')
+	if (!baseDir.endsWith(File.separator))
+		baseDir = baseDir + File.separator
+	return baseDir
+}
