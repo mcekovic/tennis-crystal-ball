ae363015b052573f9ab2ab4fc2bd5b3e4e812bc5
Mileta Cekovic <mileta.cekovic@igt.com>
2015-07-03 18:46:29 +0200

    GOAT List enhancements

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/controler/GOATController.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/controler/GOATController.java
@@ -13,8 +13,22 @@ public class GOATController {
 	private static final int PLAYER_COUNT = 1000;
 
 	private static final String GOAT_LIST_QUERY =
-		"SELECT rank() OVER (ORDER BY goat_points DESC NULLS LAST) AS goat_ranking, name, goat_points FROM player_v " +
-		"ORDER BY goat_points DESC NULLS LAST, name LIMIT ? OFFSET ?";
+		"WITH goat_list AS (" +
+			"SELECT player_id, rank() OVER (ORDER BY goat_points DESC NULLS LAST) AS goat_ranking, name, goat_points FROM player_v " +
+			"ORDER BY goat_points DESC NULLS LAST, name LIMIT ? OFFSET ?" +
+		"), titles AS (" +
+			"SELECT player_id, level, count(*) AS titles FROM tournament_event_player_result " +
+			"LEFT JOIN tournament_event USING (tournament_event_id) " +
+			"WHERE result = 'W' " +
+			"GROUP BY player_id, level" +
+		") " +
+		"SELECT goat_ranking, name, goat_points, " +
+			"(SELECT titles FROM titles t WHERE t.player_id = g.player_id AND t.level = 'G') AS grand_slams, " +
+			"(SELECT titles FROM titles t WHERE t.player_id = g.player_id AND t.level = 'F') AS tour_finals, " +
+			"(SELECT titles FROM titles t WHERE t.player_id = g.player_id AND t.level = 'M') AS masters, " +
+			"(SELECT titles FROM titles t WHERE t.player_id = g.player_id AND t.level = 'O') AS olympics, " +
+			"(SELECT sum(titles) FROM titles t WHERE t.player_id = g.player_id) AS titles " +
+		"FROM goat_list g";
 
 	@RequestMapping("/goatTable")
 	public BootgridTable<GOATListRow> goatTable(
@@ -28,7 +42,13 @@ public class GOATController {
 				int goatRanking = rs.getInt("goat_ranking");
 				String name = rs.getString("name");
 				int goatPoints = rs.getInt("goat_points");
-				table.addRow(new GOATListRow(goatRanking, name, goatPoints));
+				GOATListRow row = new GOATListRow(goatRanking, name, goatPoints);
+				row.setGrandSlams(rs.getInt("grand_slams"));
+				row.setTourFinals(rs.getInt("tour_finals"));
+				row.setMasters(rs.getInt("masters"));
+				row.setOlympics(rs.getInt("olympics"));
+				row.setTitles(rs.getInt("titles"));
+				table.addRow(row);
 			},
 			rowCount, (current-1)*rowCount
 		);
--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/GOATListRow.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/GOATListRow.java
@@ -5,6 +5,11 @@ public class GOATListRow {
 	private int goatRanking;
 	private String name;
 	private int goatPoints;
+	private int grandSlams;
+	private int tourFinals;
+	private int masters;
+	private int olympics;
+	private int titles;
 
 	public GOATListRow(int goatRanking, String name, int goatPoints) {
 		this.goatRanking = goatRanking;
@@ -22,5 +27,57 @@ public class GOATListRow {
 
 	public int getGoatPoints() {
 		return goatPoints;
+	}
+
+	public void setGoatRanking(int goatRanking) {
+		this.goatRanking = goatRanking;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setGoatPoints(int goatPoints) {
+		this.goatPoints = goatPoints;
+	}
+
+	public int getGrandSlams() {
+		return grandSlams;
+	}
+
+	public void setGrandSlams(int grandSlams) {
+		this.grandSlams = grandSlams;
+	}
+
+	public int getTourFinals() {
+		return tourFinals;
+	}
+
+	public void setTourFinals(int tourFinals) {
+		this.tourFinals = tourFinals;
+	}
+
+	public int getMasters() {
+		return masters;
+	}
+
+	public void setMasters(int masters) {
+		this.masters = masters;
+	}
+
+	public int getOlympics() {
+		return olympics;
+	}
+
+	public void setOlympics(int olympics) {
+		this.olympics = olympics;
+	}
+
+	public int getTitles() {
+		return titles;
+	}
+
+	public void setTitles(int titles) {
+		this.titles = titles;
 	}
 }
