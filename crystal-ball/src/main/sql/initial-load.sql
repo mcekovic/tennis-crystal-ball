f5da502e3066990da9d655b7347a3671f6d67d86
Mileta Cekovic <mileta.cekovic@igt.com>
2015-07-06 12:36:32 +0200

    Tweaks

--- crystal-ball/src/main/sql/initial-load.sql
+++ crystal-ball/src/main/sql/initial-load.sql
@@ -1,39 +1,44 @@
+INSERT INTO tournament_rank_points
+(level, result, rank_points, rank_points_2008, goat_points)
+VALUES
 -- Grand Slam
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'W',   2000, 1000,  200);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'F',   1200,  700,  120);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'SF',   720,  450,   72);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'QF',   360,  250,   36);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'R16',  180,  150,   18);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'R32',   90,   75,    9);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'R64',   45,   35,    4);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('G', 'R128',  10,    5, NULL);
+('G', 'W',   2000, 1000,  200),
+('G', 'F',   1200,  700,  120),
+('G', 'SF',   720,  450,   72),
+('G', 'QF',   360,  250,   36),
+('G', 'R16',  180,  150,   18),
+('G', 'R32',   90,   75,    9),
+('G', 'R64',   45,   35,    4),
+('G', 'R128',  10,    5, NULL),
 -- Tour Finals
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('F', 'W',   NULL, NULL,  150);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('F', 'F',    500,  250,  100);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('F', 'SF',   400,  200,   60);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('F', 'RR',   200,  100,   20);
+('F', 'W',   NULL, NULL,  150),
+('F', 'F',    500,  250,  100),
+('F', 'SF',   400,  200,   60),
+('F', 'RR',   200,  100,   20),
 -- Masters
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('M', 'W',   1000,  500,  100);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('M', 'F',    600,  350,   60);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('M', 'SF',   360,  225,   36);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('M', 'QF',   180,  125,   18);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('M', 'R16',   90,   75,    9);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('M', 'R32',   45,   35,    4);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('M', 'R64',   10,    5, NULL);
+('M', 'W',   1000,  500,  100),
+('M', 'F',    600,  350,   60),
+('M', 'SF',   360,  225,   36),
+('M', 'QF',   180,  125,   18),
+('M', 'R16',   90,   75,    9),
+('M', 'R32',   45,   35,    4),
+('M', 'R64',   10,    5, NULL),
 -- Olympics
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'W',    750,  400,   75);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'F',    450,  280,   45);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'BR',   340,  205,   34);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'SF',   270,  155,   27);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'QF',   135,  100,   13);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'R16',   70,   50,    7);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'R32',   35,   25,    3);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('O', 'R64',    5,    5, NULL);
+('O', 'W',    750,  400,   75),
+('O', 'F',    450,  280,   45),
+('O', 'BR',   340,  205,   34),
+('O', 'SF',   270,  155,   27),
+('O', 'QF',   135,  100,   13),
+('O', 'R16',   70,   50,    7),
+('O', 'R32',   35,   25,    3),
+('O', 'R64',    5,    5, NULL),
 -- ATP
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('A', 'W',    500,  250,   50);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('A', 'F',    300,  175,   30);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('A', 'SF',   180,  110,   18);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('A', 'QF',    90,   60,    9);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('A', 'R16',   45,   25,    4);
-INSERT INTO tournament_rank_points (level, result, rank_points, rank_points_2008, goat_points) VALUES ('A', 'R32', NULL, NULL, NULL);
+('A', 'W',    500,  250,   50),
+('A', 'F',    300,  175,   30),
+('A', 'SF',   180,  110,   18),
+('A', 'QF',    90,   60,    9),
+('A', 'R16',   45,   25,    4),
+('A', 'R32', NULL, NULL, NULL);
+
+COMMIT;
 
