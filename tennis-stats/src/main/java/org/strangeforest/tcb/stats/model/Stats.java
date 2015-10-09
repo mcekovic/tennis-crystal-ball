e4d686875881d0526a6b3599c5f79d202dc3e26b
Mileta Cekovic <mileta.cekovic@igt.com>
2015-10-09 13:00:43 +0200

    Match stats finished

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/Stats.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/Stats.java
@@ -5,26 +5,64 @@ public class Stats {
 	private final int aces;
 	private final int doubleFaults;
 	private final int servicePoints;
-	private final int firstServeIn;
-	private final int firstServeWon;
-	private final int secondServeWon;
+	private final int firstServesIn;
+	private final int firstServesWon;
+	private final int secondServesWon;
 	private final int serviceGames;
 	private final int breakPointsSaved;
 	private final int breakPointsFaced;
+
+	private final double acePct;
+	private final double doubleFaultPct;
+	private final double firstServePct;
+	private final double firstServeWonPct;
+	private final int firstServesLost;
+	private final double firstServeLostPct;
+	private final int secondServes;
+	private final int secondServesIn;
+	private final double secondServeWonPct;
+	private final int secondServesLost;
+	private final double secondServeLostPct;
+	private final int servicePointsWon;
+	private final double servicePointsWonPct;
+	private final int servicePointsLost;
+	private final double servicePointsLostPct;
+	private final Double breakPointsSavedPct;
+	private final int breakPointsLost;
+	private final Double breakPointsLostPct;
+
 	private Stats opponentStats;
 
 	private static final double PCT = 100.0;
 
-	public Stats(int aces, int doubleFaults, int servicePoints, int firstServeIn, int firstServeWon, int secondServeWon, int serviceGames, int breakPointsSaved, int breakPointsFaced) {
+	public Stats(int aces, int doubleFaults, int servicePoints, int firstServesIn, int firstServesWon, int secondServesWon, int serviceGames, int breakPointsSaved, int breakPointsFaced) {
 		this.aces = aces;
 		this.doubleFaults = doubleFaults;
 		this.servicePoints = servicePoints;
-		this.firstServeIn = firstServeIn;
-		this.firstServeWon = firstServeWon;
-		this.secondServeWon = secondServeWon;
+		this.firstServesIn = firstServesIn;
+		this.firstServesWon = firstServesWon;
+		this.secondServesWon = secondServesWon;
 		this.serviceGames = serviceGames;
 		this.breakPointsSaved = breakPointsSaved;
 		this.breakPointsFaced = breakPointsFaced;
+		acePct = servicePoints != 0 ? PCT * aces / servicePoints : 0.0;
+		doubleFaultPct = servicePoints != 0 ? PCT * doubleFaults / servicePoints : 0.0;
+		firstServePct = servicePoints != 0 ? PCT * firstServesIn / servicePoints : 0.0;
+		firstServeWonPct = firstServesIn != 0 ? PCT * firstServesWon / firstServesIn : 0.0;
+		firstServesLost = firstServesIn - firstServesWon;
+		firstServeLostPct = PCT - firstServeWonPct;
+		secondServes = servicePoints - firstServesIn;
+		secondServesIn = secondServes - doubleFaults;
+		secondServeWonPct = secondServes != 0 ? PCT * secondServesWon / secondServes : 0.0;
+		secondServesLost = secondServes - secondServesWon;
+		secondServeLostPct = PCT - secondServeWonPct;
+		servicePointsWon = firstServesWon + secondServesWon;
+		servicePointsWonPct = servicePoints != 0 ? PCT * servicePointsWon / servicePoints : 0.0;
+		servicePointsLost = firstServesLost + secondServesLost;
+		servicePointsLostPct = PCT - servicePointsWonPct;
+		breakPointsSavedPct = breakPointsFaced != 0 ? PCT * breakPointsSaved / breakPointsFaced : null;
+		breakPointsLost = breakPointsFaced - breakPointsSaved;
+		breakPointsLostPct = breakPointsFaced != 0 ? PCT * breakPointsLost / breakPointsFaced : null;
 	}
 
 
@@ -35,7 +73,7 @@ public class Stats {
 	}
 
 	public double getAcePct() {
-		return servicePoints != 0 ? PCT * aces / servicePoints : 0.0;
+		return acePct;
 	}
 
 	public int getDoubleFaults() {
@@ -43,52 +81,55 @@ public class Stats {
 	}
 
 	public double getDoubleFaultPct() {
-		return servicePoints != 0 ? PCT * doubleFaults / servicePoints : 0.0;
+		return doubleFaultPct;
 	}
 
 	public int getServicePoints() {
 		return servicePoints;
 	}
 
-	public int getFirstServeIn() {
-		return firstServeIn;
+	public int getFirstServesIn() {
+		return firstServesIn;
 	}
 
 	public double getFirstServePct() {
-		return servicePoints != 0 ? PCT * firstServeIn / servicePoints : 0.0;
+		return firstServePct;
 	}
 
-	public int getFirstServeWon() {
-		return firstServeWon;
+	public int getFirstServesWon() {
+		return firstServesWon;
+	}
+
+	public int getFirstServesLost() {
+		return firstServesLost;
 	}
 
 	public double getFirstServeWonPct() {
-		return firstServeIn != 0 ? PCT * firstServeWon / firstServeIn : 0.0;
+		return firstServeWonPct;
 	}
 
 	public int getSecondServes() {
-		return servicePoints - firstServeIn;
+		return secondServes;
 	}
 
-	public int getSecondServeIn() {
-		return getSecondServes() - doubleFaults;
+	public int getSecondServesIn() {
+		return secondServesIn;
 	}
 
-	public int getSecondServeWon() {
-		return secondServeWon;
+	public int getSecondServesWon() {
+		return secondServesWon;
 	}
 
 	public double getSecondServeWonPct() {
-		int secondServes = getSecondServes();
-		return secondServes != 0 ? PCT * secondServeWon / secondServes : 0.0;
+		return secondServeWonPct;
 	}
 
 	public int getServicePointsWon() {
-		return firstServeWon + secondServeWon;
+		return servicePointsWon;
 	}
 
 	public double getServicePointsWonPct() {
-		return servicePoints != 0 ? PCT * getServicePointsWon() / servicePoints : 0.0;
+		return servicePointsWonPct;
 	}
 
 	public int getServiceGames() {
@@ -103,6 +144,10 @@ public class Stats {
 		return breakPointsFaced;
 	}
 
+	public Double getBreakPointsSavedPct() {
+		return breakPointsSavedPct;
+	}
+
 
 	// Return
 
@@ -111,35 +156,35 @@ public class Stats {
 	}
 
 	public int getFirstServeReturnPoints() {
-		return opponentStats.firstServeIn;
+		return opponentStats.firstServesIn;
 	}
 
 	public int getFirstServeReturnPointsWon() {
-		return opponentStats.firstServeIn - opponentStats.firstServeWon;
+		return opponentStats.firstServesLost;
 	}
 
 	public double getFirstServeReturnPointsWonPct() {
-		return PCT - opponentStats.getFirstServeWonPct();
+		return opponentStats.firstServeLostPct;
 	}
 
 	public int getSecondServeReturnPoints() {
-		return opponentStats.getSecondServes();
+		return opponentStats.secondServes;
 	}
 
 	public int getSecondServeReturnPointsWon() {
-		return opponentStats.getSecondServes() - opponentStats.secondServeWon;
+		return opponentStats.secondServesLost;
 	}
 
 	public double getSecondServeReturnPointsWonPct() {
-		return PCT - opponentStats.getSecondServeWonPct();
+		return opponentStats.secondServeLostPct;
 	}
 
 	public int getReturnPointsWon() {
-		return getFirstServeReturnPointsWon() + getSecondServeReturnPointsWon();
+		return opponentStats.servicePointsLost;
 	}
 
 	public double getReturnPointsWonPct() {
-		return PCT - opponentStats.getServicePointsWonPct();
+		return opponentStats.servicePointsLostPct;
 	}
 
 	public int getReturnGames() {
@@ -147,27 +192,26 @@ public class Stats {
 	}
 
 	public int getBreakPointsWon() {
-		return opponentStats.breakPointsFaced - opponentStats.breakPointsSaved;
+		return opponentStats.breakPointsLost;
 	}
 
 	public int getBreakPoints() {
 		return opponentStats.breakPointsFaced;
 	}
 
-	public double getBreakPointsConverted() {
-		int breakPoints = getBreakPoints();
-		return breakPoints != 0 ? PCT * getBreakPointsWon() / breakPoints : 0.0;
+	public Double getBreakPointsConverted() {
+		return opponentStats.breakPointsLostPct;
 	}
 
 
 	// Totals
 
 	public int getTotalPoints() {
-		return servicePoints + opponentStats.servicePoints;
+		return servicePoints + getReturnPoints();
 	}
 
 	public int getTotalPointsWon() {
-		return getServicePointsWon() + getReturnPointsWon();
+		return servicePointsWon + getReturnPointsWon();
 	}
 
 	public double getTotalPointsWonPct() {
@@ -176,8 +220,7 @@ public class Stats {
 	}
 
 	public double getDominanceRatio() {
-		double servicePointsLost = opponentStats.getReturnPointsWonPct();
-		return servicePointsLost != 0.0 ? getReturnPointsWonPct() / servicePointsLost : 0.0;
+		return servicePointsLostPct != 0.0 ? getReturnPointsWonPct() / servicePointsLostPct : 0.0;
 	}
 
 
