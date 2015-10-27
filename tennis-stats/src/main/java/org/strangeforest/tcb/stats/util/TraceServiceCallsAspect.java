10a52d25484aea091227375ded6f19c489fded39
Mileta Cekovic <mileta.cekovic@igt.com>
2015-10-27 16:47:31 +0100

    Tracing

---
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/util/TraceServiceCallsAspect.java
@@ -0,0 +1,28 @@
+package org.strangeforest.tcb.stats.util;
+
+import org.aopalliance.aop.*;
+import org.springframework.aop.*;
+import org.springframework.aop.interceptor.*;
+import org.springframework.aop.support.*;
+import org.springframework.aop.support.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.*;
+
+@Configuration
+public class TraceServiceCallsAspect {
+
+	@Bean
+	public Advice traceInterceptor() {
+		CustomizableTraceInterceptor traceInterceptor = new CustomizableTraceInterceptor();
+		traceInterceptor.setUseDynamicLogger(true);
+		traceInterceptor.setEnterMessage("$[methodName]($[arguments])");
+		traceInterceptor.setExitMessage("~$[methodName]=$[returnValue] ($[invocationTime])");
+		return traceInterceptor;
+	}
+
+	@Bean
+	public Advisor serviceTraceAdvisor() {
+		Pointcut servicePointcut = new AnnotationMatchingPointcut(Service.class);
+		return new DefaultPointcutAdvisor(servicePointcut, traceInterceptor());
+	}
+}
