6bd7aa18f076407aec0c5435203a88abcbc6bd0e
Mileta Cekovic <mileta.cekovic@igt.com>
2015-11-05 14:27:29 +0100

    Cosmetics

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/util/ResultSetUtil.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/util/ResultSetUtil.java
@@ -2,7 +2,7 @@ package org.strangeforest.tcb.stats.util;
 
 import java.sql.*;
 
-public class ResultSetUtil {
+public abstract class ResultSetUtil {
 
 	public static Integer getInteger(ResultSet rs, String column) throws SQLException {
 		int i = rs.getInt(column);
