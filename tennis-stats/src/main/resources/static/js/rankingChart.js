5bda49ebf8396e1da12743c79a646ae78e022da8
Mileta Cekovic <mileta.cekovic@igt.com>
2015-11-18 14:20:47 +0100

    Player rankings in player profile - smart legend position
    Former-commit-id: 2c76deb410920b02a76d339b271099aa8174a4f3

--- tennis-stats/src/main/resources/static/js/rankingChart.js
+++ tennis-stats/src/main/resources/static/js/rankingChart.js
@@ -1,11 +1,11 @@
-function showChart(chartData, elementId, width, isRank, useLogScale, showLegend) {
+function showChart(chartData, elementId, width, isRank, useLogScale, legendPosition) {
 	if (chartData != undefined) {
 		var options = {
 			width: width,
 			height: width / 2,
 			chartArea: {left: 50, top: 20, height: "80%"},
 			vAxis: {direction: isRank ? -1 : 1, viewWindow: {min: isRank ? 1 : undefined}, logScale: useLogScale},
-			legend: showLegend ? undefined : {position: "none"}
+			legend: {position: legendPosition}
 		};
 		var chart = new google.visualization.LineChart(document.getElementById(elementId));
 		chart.draw(chartData, options);
