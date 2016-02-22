dba2f2417a7c276156f4a21ec1a115d726bfc65e
Mileta Cekovic <mileta.cekovic@gtech.com>
2016-02-22 22:04:07 +0100

    Support for down for maintenance
    Former-commit-id: b854d1ff71ca094c89260f249696f7e9795423b7

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/controler/TennisStatsController.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/controler/TennisStatsController.java
@@ -1,14 +1,18 @@
 package org.strangeforest.tcb.stats.controler;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TennisStatsController extends BaseController {
 
+	@Value("${tennis-stats.down-for-maintenance}")
+	private boolean downForMaintenance;
+
 	@RequestMapping("/")
 	public String index() {
-		return "index";
+		return downForMaintenance ? "maintenance" : "index";
 	}
 
 	@RequestMapping("/goatList")
