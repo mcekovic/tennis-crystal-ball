08cd7a6bff2fe474f95a0774ffede5fc5c3c2133
Mileta Cekovic <mileta.cekovic@igt.com>
2016-04-06 10:53:00 +0200

    Bug fix
    Former-commit-id: 94f2c50d63914974682e827b6e3526bc862d42f7

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/controler/PlayerRivalriesResource.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/controler/PlayerRivalriesResource.java
@@ -23,7 +23,7 @@ public class PlayerRivalriesResource {
 		put("matches", "matches");
 		put("won", "won");
 		put("lost", "lost");
-		put("wonPctStr", "won::real/(won + lost)");
+		put("wonPctStr", "CASE WHEN won + lost > 0 THEN won::real/(won + lost) ELSE 0 END");
 	}};
 	private static final OrderBy[] DEFAULT_ORDERS = new OrderBy[] {desc("matches"), desc("won")};
 
