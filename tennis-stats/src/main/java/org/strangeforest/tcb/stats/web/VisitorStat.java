47f03286c4cdef91f787160f3ffc4bc1321c9b67
Mileta Cekovic <mileta.cekovic@igt.com>
2016-04-12 14:28:24 +0200

    Cosmetics
    Former-commit-id: 9f5a9e4ea79b369cbf7f51c5602bc9e2ffad23d3

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/web/VisitorStat.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/web/VisitorStat.java
@@ -4,9 +4,9 @@ public enum VisitorStat {
 
 	VISITS("count(visitor_id)", "Visits"),
 	ACTIVE_VISITORS("sum(CASE WHEN active THEN 1 ELSE 0 END)", "Active visitors"),
+	VISITS_PER_IP("round(count(visitor_id)::NUMERIC / count(DISTINCT ip_address), 2)", "Visits per IP"),
 	HITS("sum(hits)", "Hits"),
-	HITS_PER_VISIT("avg(hits)::INTEGER", "Hits per visit"),
-	VISITS_PER_IP("round(count(visitor_id)::NUMERIC / count(DISTINCT ip_address), 2)", "Visits per IP");
+	HITS_PER_VISIT("avg(hits)::INTEGER", "Hits per visit");
 
 	private final String expression;
 	private final String caption;
