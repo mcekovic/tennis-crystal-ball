75be264d6edff58c8afe51db4418eaa9b33c4fa3
Mileta Cekovic <mileta.cekovic@gtech.com>
2016-07-05 23:25:49 +0200

    Tweaks
    Former-commit-id: 18bd506027cb009ca78ba6be9f52956e970db857

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/categories/ATPRankingCategory.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/categories/ATPRankingCategory.java
@@ -1,23 +1,21 @@
 package org.strangeforest.tcb.stats.model.records.categories;
 
-import org.strangeforest.tcb.stats.model.records.*;
-
 public class ATPRankingCategory extends RankingCategory {
 
 	private static final String ADJUSTMENT = " (adjusted by factor 1.9 before 2009)";
 
 	public ATPRankingCategory() {
 		super("ATP Ranking");
-		registerRanking("ATP", RecordCategory.N_A);
+		registerRanking("ATP", N_A);
 		register(mostPoints("ATPPoints", "Most ATP Points" + ADJUSTMENT, "player_best_rank_points", "best_rank_points_adjusted", "best_rank_points_adjusted_date", "ATP Points"));
 		register(mostEndOfSeasonPoints("EndOfSeasonATPPoints", "Most End of Season ATP Points" + ADJUSTMENT, "player_year_end_rank", "adjust_atp_rank_points(year_end_rank_points, season_start(season))", "ATP Points"));
 		register(pointsDifferenceBetweenNo1andNo2(
-			"ATPPointsNo1No2Difference", "ATP Points Difference Between No. 1 and No. 2" + ADJUSTMENT, "player_ranking", "rank_points", RecordCategory.N_A,
+			"ATPPointsNo1No2Difference", "ATP Points Difference Between No. 1 and No. 2" + ADJUSTMENT, "player_ranking", "rank_points", N_A,
 			"adjust_atp_rank_points(r1.rank_points - r2.rank_points, r1.rank_date)", "adjust_atp_rank_points(r1.rank_points, r1.rank_date)", "adjust_atp_rank_points(r2.rank_points, r1.rank_date)", "r.value DESC",
 			"numeric", null, "Points", "Points Diff."
 		));
 		register(pointsDifferenceBetweenNo1andNo2(
-			"ATPPointsNo1No2DifferencePct", "ATP Points Pct. Difference Between No. 1 and No. 2", "player_ranking", "rank_points", RecordCategory.N_A,
+			"ATPPointsNo1No2DifferencePct", "ATP Points Pct. Difference Between No. 1 and No. 2", "player_ranking", "rank_points", N_A,
 			"round(100 * (r1.rank_points - r2.rank_points) / r2.rank_points)::INTEGER", "r1.rank_points", "r2.rank_points", "r.value DESC",
 			null, "pct", "Points", "Points Pct. Diff."
 		));
--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/categories/GOATPointsCategory.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/categories/GOATPointsCategory.java
@@ -6,8 +6,9 @@ import static java.util.Arrays.*;
 
 public class GOATPointsCategory extends RecordCategory {
 
-	private static final String POINTS_WIDTH = "150";
-	private static final String SEASON_WIDTH =  "60";
+	private static final String POINTS_WIDTH =  "150";
+	private static final String SEASON_WIDTH =   "60";
+	private static final String SEASONS_WIDTH = "150";
 
 	public GOATPointsCategory() {
 		super("GOAT Points");
@@ -23,6 +24,7 @@ public class GOATPointsCategory extends RecordCategory {
 		register(mostSeasonGOATPoints("Tournament", "Tournament", "tournament_goat_points"));
 		register(mostSeasonGOATPoints("Ranking", "Ranking", "ranking_goat_points"));
 		register(mostSeasonGOATPoints("BigWins", "Big Wins", "big_wins_goat_points"));
+		register(mostConsecutiveSeasonWithGOATPoints());
 	}
 
 	private static Record mostGOATPoints(String id, String name, String columnName) {
@@ -48,6 +50,30 @@ public class GOATPointsCategory extends RecordCategory {
 			asList(
 				new RecordColumn("value", "numeric", null, POINTS_WIDTH, "right", suffix(name, " ") + "GOAT Points"),
 				new RecordColumn("season", "numeric", null, SEASON_WIDTH, "center", "Season")
+			)
+		);
+	}
+
+	private static Record mostConsecutiveSeasonWithGOATPoints() {
+		return new Record(
+         "ConsecutiveSeasonsWithGOATPoints", "Most Consecutive Seasons With at Least One GOAT Point",
+			/* language=SQL */
+			"WITH player_seasons AS (\n" +
+			"  SELECT player_id, season, season - row_number() OVER (PARTITION BY player_id ORDER BY season) AS grouping_season\n" +
+			"  FROM player_season_goat_points\n" +
+			"), player_consecutive_seasons AS (\n" +
+			"  SELECT player_id, season, grouping_season, dense_rank() OVER (PARTITION BY player_id, grouping_season ORDER BY season) AS consecutive_seasons\n" +
+			"  FROM player_seasons\n" +
+			")\n" +
+			"SELECT player_id, max(season) - max(consecutive_seasons) + 1 AS start_season, max(season) AS end_season, max(consecutive_seasons) AS value\n" +
+			"FROM player_consecutive_seasons\n" +
+			"GROUP BY player_id, grouping_season\n" +
+			"HAVING max(consecutive_seasons) > 1",
+			"r.value, r.start_season, r.end_season", "r.value DESC", "r.value DESC, r.end_season", RecordRowFactory.SEASON_RANGE_INTEGER,
+			asList(
+				new RecordColumn("value", "numeric", null, SEASONS_WIDTH, "right", "Seasons with GOAT Points"),
+				new RecordColumn("startSeason", "numeric", null, SEASON_WIDTH, "center", "Start Season"),
+				new RecordColumn("endSeason", "numeric", null, SEASON_WIDTH, "center", "End Season")
 			)
 		);
 	}
