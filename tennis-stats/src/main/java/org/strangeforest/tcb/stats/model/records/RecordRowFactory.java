222cfaaf420275f5b87bfe9cc769678e6c0e5b30
Mileta Cekovic <mileta.cekovic@igt.com>
2016-07-08 23:40:28 +0200

    Refactoring
    Former-commit-id: f2d0dd79942dd1880d3d93a9010fefe7f9cb2a36

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/RecordRowFactory.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/RecordRowFactory.java
@@ -1,119 +1,53 @@
 package org.strangeforest.tcb.stats.model.records;
 
+import java.lang.reflect.*;
+
 import org.strangeforest.tcb.stats.model.records.rows.*;
+
+import static java.lang.String.*;
 
 public enum RecordRowFactory {
 
-	INTEGER {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new IntegerRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	SEASON_INTEGER {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new SeasonIntegerRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	SEASON_RANGE_INTEGER {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new SeasonRangeIntegerRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	SEASON_TWO_INTEGERS {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new SeasonTwoIntegersRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	TOURNAMENT_INTEGER {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new TournamentIntegerRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	TOURNAMENT_EVENT_INTEGER {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new TournamentEventIntegerRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	DATE_INTEGER {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new DateIntegerRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	DATE_RANGE_INTEGER {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new DateRangeIntegerRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	DATE_AGE {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new DateAgeRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	STREAK {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new StreakRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	CAREER_SPAN {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new CareerSpanRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	TOURNAMENT_CAREER_SPAN {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new TournamentCareerSpanRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	TOURNAMENT_EVENT_AGE {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new TournamentEventAgeRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	RANKING_DIFF {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new RankingDiffRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	WINNING_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new WinningPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	LOSING_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new LosingPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	WINNING_W_DRAW_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new WinningWDrawPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	LOSING_W_DRAW_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new LosingWDrawPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	SEASON_WINNING_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new SeasonWinningPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	SEASON_LOSING_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new SeasonLosingPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	TOURNAMENT_WINNING_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new TournamentWinningPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	},
-	TOURNAMENT_LOSING_PCT {
-		@Override public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
-			return new TournamentLosingPctRecordRow(rank, playerId, name, countryId, active);
-		}
-	};
+	INTEGER(IntegerRecordRow.class),
+	SEASON_INTEGER(SeasonIntegerRecordRow.class),
+	SEASON_RANGE_INTEGER(SeasonRangeIntegerRecordRow.class),
+	SEASON_TWO_INTEGERS(SeasonTwoIntegersRecordRow.class),
+	TOURNAMENT_INTEGER(TournamentIntegerRecordRow.class),
+	TOURNAMENT_EVENT_INTEGER(TournamentEventIntegerRecordRow.class),
+	DATE_INTEGER(DateIntegerRecordRow.class),
+	DATE_RANGE_INTEGER(DateRangeIntegerRecordRow.class),
+	DATE_AGE(DateAgeRecordRow.class),
+	STREAK(StreakRecordRow.class),
+	CAREER_SPAN(CareerSpanRecordRow.class),
+	TOURNAMENT_CAREER_SPAN(TournamentCareerSpanRecordRow.class),
+	TOURNAMENT_EVENT_AGE(TournamentEventAgeRecordRow.class),
+	RANKING_DIFF(RankingDiffRecordRow.class),
+	WINNING_PCT(WinningPctRecordRow.class),
+	LOSING_PCT(LosingPctRecordRow.class),
+	WINNING_W_DRAW_PCT(WinningWDrawPctRecordRow.class),
+	LOSING_W_DRAW_PCT(LosingWDrawPctRecordRow.class),
+	SEASON_WINNING_PCT(SeasonWinningPctRecordRow.class),
+	SEASON_LOSING_PCT(SeasonLosingPctRecordRow.class),
+	TOURNAMENT_WINNING_PCT(TournamentWinningPctRecordRow.class),
+	TOURNAMENT_LOSING_PCT(TournamentLosingPctRecordRow.class);
 
-	public abstract RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active);
+	private final Constructor<? extends RecordRow> constructor;
+
+	RecordRowFactory(Class<? extends RecordRow> rowClass) {
+		try {
+			this.constructor = rowClass.getDeclaredConstructor(int.class, int.class, String.class, String.class, Boolean.class);
+		}
+		catch (NoSuchMethodException ex) {
+			throw new IllegalArgumentException(format("RecordRow class %s does not have adequate constructor.", rowClass.getName()));
+		}
+	}
+
+	public RecordRow createRow(int rank, int playerId, String name, String countryId, Boolean active) {
+		try {
+			return constructor.newInstance(rank, playerId, name, countryId, active);
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException(format("RecordRow %s cannot be instantiated.", constructor.getDeclaringClass().getName()), ex);
+		}
+	}
 }
