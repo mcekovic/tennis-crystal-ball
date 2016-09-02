e4c8fd8a70c6911e1f1e115f933d74350a7750e3
Mileta Cekovic <mileta.cekovic@igt.com>
2016-09-02 17:33:17 +0200

    Cosmetics

--- tennis-stats/src/integration-test/java/org/strangeforest/tcb/stats/web/VisitorITsConfig.java
+++ tennis-stats/src/integration-test/java/org/strangeforest/tcb/stats/web/VisitorITsConfig.java
@@ -2,7 +2,6 @@ package org.strangeforest.tcb.stats.web;
 
 import org.springframework.boot.test.context.*;
 import org.springframework.context.annotation.*;
-import org.springframework.context.support.*;
 import org.springframework.transaction.annotation.*;
 import org.strangeforest.tcb.stats.*;
 import org.strangeforest.tcb.stats.spring.*;
@@ -12,11 +11,6 @@ import org.strangeforest.tcb.stats.spring.*;
 @EnableTransactionManagement
 @Import({TennisStatsConfig.class, DataSourceITConfig.class})
 public class VisitorITsConfig {
-
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}
 
 	@Bean
 	public VisitorManager visitorManager() {
