2e37590dcbb7d75d0025a136e6f378ad98c64480
Mileta Cekovic <mileta.cekovic@gtech.com>
2016-09-25 23:38:07 +0200

    Fix

---
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/controller/TournamentLevelTimelineController.java
@@ -0,0 +1,32 @@
+package org.strangeforest.tcb.stats.controller;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
+import org.strangeforest.tcb.stats.model.*;
+import org.strangeforest.tcb.stats.service.*;
+
+import static org.strangeforest.tcb.stats.model.TournamentLevel.*;
+
+@Controller
+public class TournamentLevelTimelineController extends PageController {
+
+	@Autowired
+	private TournamentLevelService tournamentLevelService;
+
+	@GetMapping("/tournamentLevelTimeline")
+	public ModelAndView tournamentLevelTimeline(
+      @RequestParam(name = "level") String level
+	) {
+		TournamentLevel tournamentLevel = TournamentLevel.decode(level);
+		TournamentLevelTimeline timeline = tournamentLevelService.getTournamentLevelTimeline(level, tournamentLevel != MASTERS);
+
+		ModelMap modelMap = new ModelMap();
+		modelMap.addAttribute("level", tournamentLevel);
+		modelMap.addAttribute("timeline", timeline);
+		return new ModelAndView("tournamentLevelTimeline", modelMap);
+	}
+}
+
