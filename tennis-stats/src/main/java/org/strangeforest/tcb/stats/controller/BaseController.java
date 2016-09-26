8ed19cfb06f64efab90fedaaf5834335e3ecf8a1
Mileta Cekovic <mileta.cekovic@gtech.com>
2016-09-26 21:02:16 +0200

    Fixes

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/controller/BaseController.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/controller/BaseController.java
@@ -1,16 +1,12 @@
 package org.strangeforest.tcb.stats.controller;
 
 import java.util.*;
-import javax.servlet.http.*;
 
-import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import com.google.common.collect.*;
 
 public abstract class BaseController {
-
-	@Autowired private HttpServletRequest request;
 
 	public static final Map<String, String> VERSIONS = ImmutableMap.of(
 		"jquery", "2.2.4",
@@ -22,10 +18,5 @@ public abstract class BaseController {
 	@ModelAttribute("versions")
 	public Map<String, String> getVersions() {
 		return VERSIONS;
-	}
-
-	@ModelAttribute("servletPath")
-	public String getServletPath() {
-		return request.getServletPath();
 	}
 }
