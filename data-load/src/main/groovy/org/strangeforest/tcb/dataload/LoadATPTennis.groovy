4fcbb393927850e77d320cece08deb3357090a17
Mileta Cekovic <mileta.cekovic@igt.com>
2016-11-14 10:56:11 +0100

    Tweaks

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennis.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennis.groovy
@@ -1,27 +1,29 @@
 package org.strangeforest.tcb.dataload
 
+import com.google.common.base.*
+
+println 'Loading Tennis Data'
+def stopwatch = Stopwatch.createStarted()
+
 def sqlPool = new SqlPool()
-
 def loader = new ATPTennisLoader()
+
 loader.loadPlayers(new PlayerLoader(sqlPool))
+sqlPool.withSql { sql -> loader.loadAdditionalPlayerData(sql) }
+
 loader.loadRankings(new RankingLoader(sqlPool))
+sqlPool.withSql { sql -> loader.loadAdditionalRankingData(sql) }
+sqlPool.withSql { sql -> LoadAdHocRankings.loadRankings(sql) }
+
 loader.loadMatches(new MatchLoader(sqlPool))
-
-sqlPool.withSql { sql ->
-	loader.loadAdditionalPlayerData(sql)
-	loader.loadAdditionalRankingData(sql)
-	LoadAdHocRankings.loadRankings(sql)
-	loader.loadAdditionalTournamentData(sql)
-}
-
+sqlPool.withSql { sql -> loader.loadAdditionalTournamentData(sql) }
 LoadAdHocTournaments.loadTournaments(sqlPool)
 
 sqlPool.withSql { sql -> loader.correctData(sql) }
-
 new EloRatings(sqlPool).compute(true)
-
 sqlPool.withSql { sql -> loader.refreshMaterializedViews(sql) }
-
 new RecordsLoader().loadRecords()
 
 sqlPool.withSql { sql -> loader.vacuum(sql) }
+
+println "Tennis Data loaded in $stopwatch"
--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennisAdHoc.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennisAdHoc.groovy
@@ -1,7 +1,11 @@
 package org.strangeforest.tcb.dataload
 
-def sqlPool = new SqlPool()
+import com.google.common.base.*
 
+println 'Loading Tennis Data'
+def stopwatch = Stopwatch.createStarted()
+
+def sqlPool = new SqlPool()
 def loader = new ATPTennisLoader()
 
 sqlPool.withSql { sql -> LoadAdHocRankings.loadRankings(sql) }
@@ -9,11 +13,10 @@ sqlPool.withSql { sql -> LoadAdHocRankings.loadRankings(sql) }
 LoadAdHocTournaments.loadTournaments(sqlPool)
 
 sqlPool.withSql { sql -> loader.correctData(sql) }
-
 new EloRatings(sqlPool).compute(save = true, fullSave = false)
-
 sqlPool.withSql { sql -> loader.refreshMaterializedViews(sql) }
-
 new RecordsLoader().loadRecords()
 
 sqlPool.withSql { sql -> loader.vacuum(sql) }
+
+println "Tennis Data loaded in $stopwatch"
