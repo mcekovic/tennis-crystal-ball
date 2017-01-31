1936b014af57ec60c20363bee3756b392deae0bb
Mileta Cekovic <mileta.cekovic@igt.com>
2017-01-31 15:21:19 +0100

    Tweak

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/Main.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/Main.groovy
@@ -3,6 +3,7 @@ package org.strangeforest.tcb.dataload
 def cli = new CliBuilder(usage: 'data-load [commands]', header: 'Commands:')
 cli.c('Create database objects')
 cli.d('Drop database objects')
+cli.p('Load additional player data')
 cli.a('Load add-hoc rankings and tournaments')
 cli.e('Compute Elo ratings')
 cli.r('Refresh computed data')
@@ -10,11 +11,13 @@ cli.rc('Refresh Records')
 cli.v('Vacuum space')
 def options = cli.parse(args)
 
-if (options && (options.c || options.d || options.a || options.e || options.r || options.rc || options.v)) {
+if (options && (options.c || options.d || options.p || options.a || options.e || options.r || options.rc || options.v)) {
 	if (options.c)
 		callLoader('createDatabase')
 	if (options.d)
 		callLoader('dropDatabase')
+	if (options.p)
+		callLoader('loadAdditionalPlayerData')
 	if (options.a) {
 		new LoadAdHocRankings().run()
 		new LoadAdHocTournaments().run()
