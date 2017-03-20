b6ce807aa0af8be87ce14c7d111c0d44091aa799
Mileta Cekovic <mileta.cekovic@igt.com>
2017-03-20 15:42:42 +0100

    Load tweak

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennisAdHoc.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadATPTennisAdHoc.groovy
@@ -15,6 +15,9 @@ LoadAdHocTournaments.loadTournaments(sqlPool)
 sqlPool.withSql { sql -> loader.correctData(sql) }
 new EloRatings(sqlPool).compute(save = true, fullSave = false)
 sqlPool.withSql { sql -> loader.refreshMaterializedViews(sql) }
+
+sqlPool.withSql { sql -> loader.vacuum(sql) }
+
 new RecordsLoader().loadRecords()
 
 sqlPool.withSql { sql -> loader.vacuum(sql) }
