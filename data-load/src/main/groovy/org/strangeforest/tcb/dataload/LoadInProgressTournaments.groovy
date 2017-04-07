cbe179955a29664e3b3db6c0103b3fcbef45c22c
Mileta Cekovic <mileta.cekovic@gtech.com>
2017-04-07 12:21:42 +0200

    Cosmetics

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadInProgressTournaments.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadInProgressTournaments.groovy
@@ -5,6 +5,7 @@ loadTournaments(new SqlPool())
 static loadTournaments(SqlPool sqlPool) {
 	sqlPool.withSql {sql ->
 		def atpInProgressTournamentLoader = new ATPWorldTourInProgressTournamentLoader(sql)
+		atpInProgressTournamentLoader.loadAndSimulateTournament('indian-wells', 404)
 		atpInProgressTournamentLoader.loadAndSimulateTournament('miami', 403)
 	}
 }
