2bf4714d935a5583de20ececac2a01ea7275f5f5
Mileta Cekovic <mileta.cekovic@gtech.com>
2017-07-30 09:16:12 +0200

    Fix in-progress tournament crawler

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadInProgressTournaments.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/LoadInProgressTournaments.groovy
@@ -1,6 +1,7 @@
 package org.strangeforest.tcb.dataload
 
-import static org.strangeforest.tcb.dataload.LoaderUtil.*
+import static java.util.Arrays.asList
+import static org.strangeforest.tcb.dataload.LoaderUtil.retriedGetDoc
 
 loadTournaments(new SqlPool())
 
@@ -9,7 +10,7 @@ static loadTournaments(SqlPool sqlPool) {
 		def atpInProgressTournamentLoader = new ATPWorldTourInProgressTournamentLoader(sql)
 		def oldExtIds = atpInProgressTournamentLoader.findInProgressEventExtIds()
 		println "Old in-progress tournaments: $oldExtIds"
-		def eventInfos = findInProgressEvents('/en/scores/current')
+		def eventInfos = findInProgressEvents()
 		def newExtIds = eventInfos.collect { info -> info.extId }
 		println "New in-progress tournaments: $newExtIds"
 		eventInfos.each { info ->
@@ -23,19 +24,58 @@ static loadTournaments(SqlPool sqlPool) {
 	}
 }
 
-static findInProgressEvents(String url, boolean processUrls = true) {
+static findInProgressEvents() {
+	def eventInfos = new TreeSet<>()
+	def crawler = new CascadingCrawler('/en/scores/current')
+	crawler.visit { url -> eventInfos.addAll findInProgressEvents(url, crawler) }
+	eventInfos.removeIf { info -> !info.extId }
+	eventInfos
+}
+
+static findInProgressEvents(String url, CascadingCrawler crawler) {
 	def doc = retriedGetDoc('http://www.atpworldtour.com' + url)
 	def eventInfos = new TreeSet<>()
 	eventInfos.addAll doc.select('div.arrow-next-tourney > div > a.tourney-title').collect { a ->
 		def eventUrl = a.attr('href')
-		if (processUrls)
-			eventInfos.addAll findInProgressEvents(eventUrl, false)
+		crawler.addUrl eventUrl
 		new EventInfo(eventUrl)
 	}
 	def eventUrl = doc.select('div.module-header > div.module-tabs > div.module-tab.current > span > a').attr('href')
+	crawler.addUrl eventUrl
 	eventInfos << new EventInfo(eventUrl)
-	if (processUrls)
-		eventInfos.addAll findInProgressEvents(eventUrl, false)
-	eventInfos.removeIf { info -> !info.extId }
 	eventInfos
+}
+
+class CascadingCrawler {
+
+	Set<String> visitedUrls
+	Set<String> urls
+
+	CascadingCrawler(String... urls) {
+		visitedUrls = new HashSet<>()
+		this.urls = new HashSet<>(asList(urls))
+	}
+
+	def visit(Closure c) {
+		while (!urls.empty) {
+			for (String url : new ArrayList<>(urls)) {
+				c(url)
+				visited url
+			}
+		}
+	}
+
+	def isVisited(String url) {
+		visitedUrls.contains(url)
+	}
+
+	def visited(String url) {
+		visitedUrls.add url
+		urls.remove url
+	}
+
+	def addUrl(String url) {
+		if (!isVisited(url))
+			urls.add url
+	}
 }
