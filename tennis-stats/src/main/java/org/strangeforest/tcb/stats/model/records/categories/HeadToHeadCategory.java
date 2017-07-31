7f8efe2d15de0957c9b98071af7ac93bd2a9f71c
Mileta Cekovic <mileta.cekovic@igt.com>
2017-07-31 16:29:52 +0200

    H2H records vs once No1, Top5 and Top10

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/categories/HeadToHeadCategory.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/records/categories/HeadToHeadCategory.java
@@ -3,7 +3,9 @@ package org.strangeforest.tcb.stats.model.records.categories;
 import org.strangeforest.tcb.stats.model.records.*;
 import org.strangeforest.tcb.stats.model.records.details.*;
 
+import static java.lang.String.*;
 import static java.util.Arrays.*;
+import static org.strangeforest.tcb.stats.model.records.RecordDomain.*;
 import static org.strangeforest.tcb.stats.model.records.categories.HeadToHeadCategory.MostLeast.*;
 import static org.strangeforest.tcb.stats.model.records.categories.HeadToHeadCategory.PctRecordType.*;
 import static org.strangeforest.tcb.stats.model.records.categories.HeadToHeadCategory.RecordType.*;
@@ -66,7 +68,7 @@ public class HeadToHeadCategory extends RecordCategory {
 	private static final RecordColumn WON_COLUMN = new RecordColumn("won", "numeric", null, H2H_SMALL_WIDTH, "right", "Won");
 	private static final RecordColumn LOST_COLUMN = new RecordColumn("lost", "numeric", null, H2H_SMALL_WIDTH, "right", "Lost");
 
-	private static final String NOTES = "Minimum 3 matches in H2H series, minimum 10 H2H series";
+	private static final String NOTES = "Minimum 3 matches in H2H series, minimum %1$d H2H series";
 
 	public HeadToHeadCategory(boolean infamous) {
 		super((infamous ? "Infamous " : "") + "Head-to-Head");
@@ -76,6 +78,15 @@ public class HeadToHeadCategory extends RecordCategory {
 			register(mostH2HSeries(LEAST, LOST));
 			register(mostH2HSeries(MOST, DRAW));
 			register(greatestH2HSeriesPct(WINNING));
+			register(mostH2HSeriesVs(MOST, PLAYED, NO_1_FILTER));
+			register(mostH2HSeriesVs(MOST, PLAYED, TOP_5_FILTER));
+			register(mostH2HSeriesVs(MOST, PLAYED, TOP_10_FILTER));
+			register(mostH2HSeriesVs(MOST, WON, NO_1_FILTER));
+			register(mostH2HSeriesVs(MOST, WON, TOP_5_FILTER));
+			register(mostH2HSeriesVs(MOST, WON, TOP_10_FILTER));
+			register(greatestH2HSeriesPctVs(WINNING, NO_1_FILTER));
+			register(greatestH2HSeriesPctVs(WINNING, TOP_5_FILTER));
+			register(greatestH2HSeriesPctVs(WINNING, TOP_10_FILTER));
 		}
 		else {
 			register(mostH2HSeries(MOST, LOST));
@@ -85,26 +96,28 @@ public class HeadToHeadCategory extends RecordCategory {
 	}
 
 	private static Record mostH2HSeries(MostLeast mostLeast, RecordType type) {
+		int minSeries = 10;
 		return new Record<>(
 			mostLeast.name + "H2HSeries" + type.name, mostLeast.name + " Head-to-Head Series " + type.name,
 			/* language=SQL */
 			"SELECT player_id, " + type.column + " AS value\n" +
 			"FROM player_h2h\n" +
-			"WHERE " + HTH_TOTAL + " >= 10",
+			"WHERE " + HTH_TOTAL + " >= " + minSeries,
 			"r.value", mostLeast.order, mostLeast.order,
 			IntegerRecordDetail.class, null,
 			asList(new RecordColumn("value", "numeric", null, H2H_WIDTH, "right", "H2H Series " + type.name)),
-			NOTES
+			notes(minSeries)
 		);
 	}
 
 	private static Record greatestH2HSeriesPct(PctRecordType type) {
+		int minSeries = 10;
 		return new Record<>(
 			"H2HSeries" + type.name + "Pct", "Greatest Head-to-Head Series " + type.name + " Pct.",
 			/* language=SQL */
 			"SELECT player_id, " + type.expression + " AS pct, h2h_won AS won, h2h_draw AS draw, h2h_lost AS lost\n" +
 			"FROM player_h2h\n" +
-			"WHERE " + HTH_TOTAL + " >= 10",
+			"WHERE " + HTH_TOTAL + " >= " + minSeries,
 			"r.won, r.draw, r.lost", "r.pct DESC", "r.pct DESC, r.won + r.draw + r.lost DESC",
 			type.detailClass, null,
 			asList(
@@ -114,7 +127,80 @@ public class HeadToHeadCategory extends RecordCategory {
 				type.value2RecordColumn,
 				new RecordColumn("played", "numeric", null, H2H_SMALL_WIDTH, "right", "Played")
 			),
-			NOTES
+			notes(minSeries)
 		);
+	}
+
+	private static Record mostH2HSeriesVs(MostLeast mostLeast, RecordType type, RecordDomain domain) {
+		int minSeries = minSeries(domain);
+		return new Record<>(
+			mostLeast.name + "H2HSeries" + type.name + "Vs" + domain.id, mostLeast.name + " Head-to-Head Series " + type.name + " Vs. Once " + domain.name,
+			/* language=SQL */
+			h2hVsSql(domain) +
+			"SELECT player_id, " + type.column + " AS value\n" +
+			"FROM player_h2h\n" +
+			"WHERE " + HTH_TOTAL + " >= " + minSeries,
+			"r.value", mostLeast.order, mostLeast.order,
+			IntegerRecordDetail.class, null,
+			asList(new RecordColumn("value", "numeric", null, H2H_WIDTH, "right", "H2H Series " + type.name)),
+         notes(minSeries)
+		);
+	}
+
+	private static Record greatestH2HSeriesPctVs(PctRecordType type, RecordDomain domain) {
+		int minSeries = minSeries(domain);
+		return new Record<>(
+			"H2HSeries" + type.name + "PctVs" + domain.id, "Greatest Head-to-Head Series " + type.name + " Pct. Vs. Once " + domain.name,
+			/* language=SQL */
+			h2hVsSql(domain) +
+			"SELECT player_id, " + type.expression + " AS pct, h2h_won AS won, h2h_draw AS draw, h2h_lost AS lost\n" +
+			"FROM player_h2h\n" +
+			"INNER JOIN player_best_rank USING (player_id)\n" +
+			"WHERE best_rank " + domain.condition + " AND " + HTH_TOTAL + " >= " + minSeries,
+			"r.won, r.draw, r.lost", "r.pct DESC", "r.pct DESC, r.won + r.draw + r.lost DESC",
+			type.detailClass, null,
+			asList(
+				new RecordColumn("value", null, null, PCT_WIDTH, "right", "H2H " + type.name + " Pct."),
+				type.value1RecordColumn,
+				new RecordColumn("draw", "numeric", null, H2H_SMALL_WIDTH, "right", "Draw"),
+				type.value2RecordColumn,
+				new RecordColumn("played", "numeric", null, H2H_SMALL_WIDTH, "right", "Played")
+			),
+			notes(minSeries)
+		);
+	}
+
+	private static String h2hVsSql(RecordDomain domain) {
+		/* language=SQL */
+		return "WITH rivalry AS (\n" +
+			"  SELECT m.player_id, m.opponent_id, sum(m.p_matches) AS p_matches, sum(m.o_matches) AS o_matches\n" +
+			"  FROM player_match_for_stats_v m\n" +
+			"  INNER JOIN player_best_rank r ON r.player_id = m.opponent_id\n" +
+			"  WHERE r.best_rank " + domain.condition + "\n" +
+			"  GROUP BY m.player_id, m.opponent_id\n" +
+			"  HAVING count(match_id) >= 3\n" +
+			"), player_h2h AS (\n" +
+			"  SELECT player_id,\n" +
+			"    sum(CASE WHEN p_matches > o_matches THEN 1 ELSE 0 END) AS h2h_won,\n" +
+			"    sum(CASE WHEN p_matches = o_matches THEN 1 ELSE 0 END) AS h2h_draw,\n" +
+			"    sum(CASE WHEN p_matches < o_matches THEN 1 ELSE 0 END) AS h2h_lost\n" +
+			"  FROM rivalry\n" +
+			"  GROUP BY player_id\n" +
+			")\n";
+	}
+
+
+
+	// Util
+
+	private static String notes(int minSeries) {
+		return format(NOTES, minSeries);
+	}
+
+	private static int minSeries(RecordDomain domain) {
+		switch (domain) {
+			case NO_1_FILTER: return 5;
+			default: return 10;
+		}
 	}
 }
