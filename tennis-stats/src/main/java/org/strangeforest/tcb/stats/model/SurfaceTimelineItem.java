63ea7127ad9c120f916543799c66c8b8238b9ba9
Mileta Cekovic <mileta.cekovic@gtech.com>
2017-11-13 00:52:58 +0100

    Revert Fix

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/SurfaceTimelineItem.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/model/SurfaceTimelineItem.java
@@ -29,15 +29,9 @@ public class SurfaceTimelineItem {
 	}
 
 	public SurfaceTimelineItem(int season, int matchCount, int hardMatchCount, int clayMatchCount, int grassMatchCount, int carpetMatchCount, int hardIndoorMatchCount) {
-		this.season = season;
-		hardPct = scaledPct(hardMatchCount, matchCount);
+		this(season, matchCount, hardMatchCount, clayMatchCount, grassMatchCount, carpetMatchCount);
 		hardOutdoorPct = scaledPct(hardMatchCount - hardIndoorMatchCount, matchCount);
 		hardIndoorPct = scaledPct(hardIndoorMatchCount, matchCount);
-		clayPct = scaledPct(clayMatchCount, matchCount);
-		grassPct = scaledPct(grassMatchCount, matchCount);
-		BigDecimal aCarpetPct = scaledPct(carpetMatchCount, matchCount);
-		BigDecimal hardClayGrassPct = hardOutdoorPct.add(hardIndoorPct).add(clayPct).add(grassPct);
-		carpetPct = hardClayGrassPct.add(aCarpetPct).compareTo(HUNDRED) <= 0 ? aCarpetPct : HUNDRED.subtract(hardClayGrassPct);
 	}
 
 	public int getSeason() {
