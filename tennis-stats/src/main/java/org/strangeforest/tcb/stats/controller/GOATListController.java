2756e1cbc205a376c6206636fdd965f648885dcf
Mileta Cekovic <mileta.cekovic@gtech.com>
2017-12-13 21:38:14 +0100

    Make Your Own GOAT!!!

--- tennis-stats/src/main/java/org/strangeforest/tcb/stats/controller/GOATListController.java
+++ tennis-stats/src/main/java/org/strangeforest/tcb/stats/controller/GOATListController.java
@@ -2,12 +2,13 @@ package org.strangeforest.tcb.stats.controller;
 
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.*;
 import org.strangeforest.tcb.stats.model.*;
-import org.strangeforest.tcb.stats.model.core.*;
+import org.strangeforest.tcb.stats.service.*;
 
 import com.google.common.collect.*;
 
@@ -15,6 +16,8 @@ import static org.strangeforest.tcb.stats.controller.ParamsUtil.*;
 
 @Controller
 public class GOATListController extends PageController {
+
+	@Autowired private GOATLegendService goatLegendService;
 
 	private static Map<Integer, String> FACTOR_MAP = ImmutableMap.<Integer, String>builder()
 		.put(0, "x 0")
@@ -79,8 +82,53 @@ public class GOATListController extends PageController {
 		modelMap.addAttribute("config", config);
 		modelMap.addAttribute("factors", FACTOR_MAP);
 		modelMap.addAttribute("levels", GOATListConfig.TOURNAMENT_LEVELS);
-		modelMap.addAttribute("levelEnum", TournamentLevel.GRAND_SLAM);
 
 		return new ModelAndView("goatList", modelMap);
+	}
+
+	@GetMapping("/goatLegend")
+	public ModelAndView goatLegend(
+		@RequestParam(name = "forSeason", required = false) boolean forSeason
+	) {
+		ModelMap modelMap = new ModelMap();
+		modelMap.addAttribute("forSeason", forSeason);
+		// Tournament
+		modelMap.addAttribute("tournamentGOATPoints", goatLegendService.getTournamentGOATPoints());
+		// Ranking
+		modelMap.addAttribute("yearEndRankGOATPoints", goatLegendService.getYearEndRankGOATPoints());
+		modelMap.addAttribute("bestRankGOATPoints", goatLegendService.getBestRankGOATPoints());
+		modelMap.addAttribute("weeksAtNo1ForGOATPoint", goatLegendService.getWeeksAtNo1ForGOATPoint());
+		modelMap.addAttribute("weeksAtEloTopNForGOATPoint", goatLegendService.getWeeksAtEloTopNGOATPoint());
+		modelMap.addAttribute("bestEloRatingGOATPoints", goatLegendService.getBestEloRatingGOATPoints());
+		modelMap.addAttribute("bestSurfaceEloRatingGOATPoints", goatLegendService.getBestSurfaceEloRatingGOATPoints());
+		modelMap.addAttribute("bestIndoorEloRatingGOATPoints", goatLegendService.getBestIndoorEloRatingGOATPoints());
+		// Achievements
+		modelMap.addAttribute("careerGrandSlamGOATPoints", goatLegendService.getCareerGrandSlamGOATPoints());
+		modelMap.addAttribute("seasonGrandSlamGOATPoints", goatLegendService.getSeasonGrandSlamGOATPoints());
+		modelMap.addAttribute("season3GrandSlamGOATPoints", goatLegendService.getSeason3GrandSlamGOATPoints());
+		modelMap.addAttribute("grandSlamHolderGOATPoints", goatLegendService.getGrandSlamHolderGOATPoints());
+		modelMap.addAttribute("consecutiveGrandSlamOnSameEventGOATPoints", goatLegendService.getConsecutiveGrandSlamOnSameEventGOATPoints());
+		modelMap.addAttribute("grandSlamOnSameEventGOATPointsDivider", (int)(1.0 / goatLegendService.getGrandSlamOnSameEventGOATPoints()));
+		modelMap.addAttribute("bigWinMatchFactors", goatLegendService.getBigWinMatchFactors());
+		modelMap.addAttribute("bigWinRankFactors", goatLegendService.getBigWinRankFactors());
+		modelMap.addAttribute("h2hRankFactors", goatLegendService.getH2hRankFactors());
+		modelMap.addAttribute("bestSeasonGOATPoints", goatLegendService.getBestSeasonGOATPoints());
+		modelMap.addAttribute("greatestRivalriesGOATPoints", goatLegendService.getGreatestRivalriesGOATPoints());
+		return new ModelAndView("goatLegend", modelMap);
+	}
+
+	@GetMapping("/recordsGOATPointsLegend")
+	public ModelAndView recordsGOATPointsLegend() {
+		return new ModelAndView("recordsGOATPointsLegend", "recordsGOATPoints", goatLegendService.getRecordsGOATPoints());
+	}
+
+	@GetMapping("/performanceGOATPointsLegend")
+	public ModelAndView performanceGOATPointsLegend() {
+		return new ModelAndView("performanceGOATPointsLegend", "performanceGOATPoints", goatLegendService.getPerformanceGOATPoints());
+	}
+
+	@GetMapping("/statisticsGOATPointsLegend")
+	public ModelAndView statisticsGOATPointsLegend() {
+		return new ModelAndView("statisticsGOATPointsLegend", "statisticsGOATPoints", goatLegendService.getStatisticsGOATPoints());
 	}
 }
