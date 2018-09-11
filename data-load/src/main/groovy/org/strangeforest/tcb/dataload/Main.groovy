efa541ef5480112c9ae36a1982090ef85773c1d0
Cekovic, Mileta <mileta.cekovic@IGT.com>
2018-09-11 13:07:43 +0200

    Tweaks

--- data-load/src/main/groovy/org/strangeforest/tcb/dataload/Main.groovy
+++ data-load/src/main/groovy/org/strangeforest/tcb/dataload/Main.groovy
@@ -5,35 +5,38 @@ import groovy.cli.commons.CliBuilder
 import static org.strangeforest.tcb.dataload.LoadParams.*
 
 def cli = new CliBuilder(usage: 'data-load [commands]', header: 'Commands:')
+cli.bd(args: 1, argName: 'Base Data Directory', 'Base Data Directory where CSV data files are located [required for -lt]')
+cli.u(args: 1, argName: 'DB username', 'Database login username [default tcb]')
+cli.p(args: 1, argName: 'DB password', 'Database login password [default tcb]')
 cli.c(args: 1, argName: 'DB connections', 'Number of database connections to allocate [default 2]')
 cli.f('Full load [default]')
 cli.d('Delta load')
-cli.cd('Create database objects')
 cli.dd('Drop database objects')
+cli.cd('Create database objects')
 cli.lt('Load all tennis data')
 cli.ln('Load new tennis data')
 cli.lp('Load additional player data')
 cli.la('Load ad-hoc rankings and tournaments')
-cli.nt('Load new completed tournaments')
 cli.nr('Load new rankings')
-cli.ip('Load in-progress tournaments')
-cli.ff('Force in-progress tournaments forecast')
+cli.nt('Load new completed tournaments')
 cli.el('Compute Elo ratings')
 cli.rc('Refresh computed data')
 cli.rr('Refresh Records')
 cli.rp(args: 1, argName: 'Records pause', 'Pause between refreshing records in ms [default 0]')
+cli.ip('Load in-progress tournaments')
+cli.ff('Force in-progress tournaments forecast')
 cli.vc('Vacuum space')
 cli.cc('Clear caches')
 cli.help('Print this message')
 def options = cli.parse(args)
 
-if (options && (options.cd || options.dd || options.lt || options.ln || options.lp || options.la || options.nt || options.nr || options.ip || options.ff || options.el || options.rc || options.rr || options.rp || options.vc || options.cc)) {
+if (options && (options.dd || options.cd || options.lt || options.ln || options.lp || options.la || options.nr || options.nt || options.el || options.rc || options.rr || options.rp || options.ip || options.ff || options.vc || options.cc)) {
 	setProperties(options)
-	
-	if (options.cd)
-		callLoader('createDatabase')
+
 	if (options.dd)
 		callLoader('dropDatabase')
+	if (options.cd)
+		callLoader('createDatabase')
 	if (options.lt)
 		new LoadATPTennis().run()
 	if (options.ln)
@@ -46,12 +49,10 @@ if (options && (options.cd || options.dd || options.lt || options.ln || options.
 		new LoadAdHocRankings().run()
 		new LoadAdHocTournaments().run()
 	}
-	if (options.nt)
-		new LoadNewTournaments().run()
 	if (options.nr)
 		new LoadNewRankings().run()
-	if (options.ip)
-		new LoadInProgressTournaments().run()
+	if (options.nt)
+		new LoadNewTournaments().run()
 	if (options.el)
 		new ComputeEloRatings().run()
 	if (options.rc) {
@@ -63,6 +64,8 @@ if (options && (options.cd || options.dd || options.lt || options.ln || options.
 			new RecordsLoader().loadRecords(new ATPTennisLoader(), sql)
 		}
 	}
+	if (options.ip)
+		new LoadInProgressTournaments().run()
 	if (options.vc)
 		callLoader('vacuum')
 	if (options.cc)
@@ -71,24 +74,25 @@ if (options && (options.cd || options.dd || options.lt || options.ln || options.
 else
 	cli.usage()
 
-def setProperties(def options) {
-	if (options.c) {
-		def dbConns = String.valueOf(options.getProperty('c')).trim()
-		System.setProperty(SqlPool.DB_CONNECTIONS_PROPERTY, dbConns)
-	}
-	
-	if (options.f)
-		System.setProperty(FULL_LOAD_PROPERTY, 'true')
-	else if (options.d)
-		System.setProperty(FULL_LOAD_PROPERTY, 'false')
+static def setProperties(def options) {
+	setProperty(options.getProperty('bd'), BASE_DIR_PROPERTY)
+	setProperty(options.getProperty('u'), SqlPool.USERNAME_PROPERTY)
+	setProperty(options.getProperty('p'), SqlPool.PASSWORD_PROPERTY)
+	setProperty(options.getProperty('c'), SqlPool.DB_CONNECTIONS_PROPERTY)
+	setBooleanProperty(options.getProperty('f'), FULL_LOAD_PROPERTY, true)
+	setBooleanProperty(options.getProperty('d'), FULL_LOAD_PROPERTY, false)
+	setBooleanProperty(options.getProperty('ff'), FORCE_FORECAST_PROPERTY, true)
+	setProperty(options.getProperty('rp'), RECORD_PAUSE_PROPERTY)
+}
 
-	if (options.ff)
-		System.setProperty(FORCE_FORECAST_PROPERTY, 'true')
-	
-	if (options.rp) {
-		def recordsPause = String.valueOf(options.getProperty('rp')).trim()
-		System.setProperty(RECORD_PAUSE_PROPERTY, recordsPause)
-	}
+static def setProperty(cmdArgument, String systemProperty) {
+	if (cmdArgument)
+		System.setProperty(systemProperty, String.valueOf(cmdArgument).trim())
+}
+
+static def setBooleanProperty(cmdArgument, String systemProperty, boolean value) {
+	if (cmdArgument)
+		System.setProperty(systemProperty, String.valueOf(value))
 }
 
 static def callLoader(methodName) {
