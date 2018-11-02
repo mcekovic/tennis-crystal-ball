7adade38c4188daaf2470d5bc68b095d9855f6e1
Cekovic, Mileta <mileta.cekovic@IGT.com>
2018-11-02 16:13:55 +0100

    Cosmetics

--- data-load/src/main/db/court-speed-util.sql
+++ data-load/src/main/db/court-speed-util.sql
@@ -1,7 +1,7 @@
-SELECT 10 * floor((court_speed + 5) / 10) speed, count(*) count, round(100.0 * count(*)::NUMERIC / (SELECT count(*) FROM event_stats), 1) pct,
+SELECT 10 * floor((court_speed + 5) / 10) speed, count(*) count, round(100.0 * count(*) / count, 1) pct,
 	round(100.0 * avg(ace_pct)::NUMERIC, 1) ace_pct, round(100.0 * avg(sv_pts_won_pct)::NUMERIC - 50, 1) sv_pts_won_pct, round(100.0 * avg(sv_gms_won_pct)::NUMERIC - 50, 1) sv_gms_won_pct
-FROM event_stats
-GROUP BY ROLLUP(speed)
+FROM event_stats, (SELECT count(*) count FROM event_stats) event_count
+GROUP BY ROLLUP(speed), count
 ORDER BY speed;
 
 SELECT round(avg(court_speed)::NUMERIC, 2) avg,
